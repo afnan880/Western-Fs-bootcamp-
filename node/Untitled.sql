86311bbc2a298b8748fcc36753a3e35ff55c7711
afnan <afnanmudsh@gmail.com>
2026-05-28 22:01:31 -0400

    sql

---
+++ node/Untitled.sql
@@ -0,0 +1,9 @@
+-- to create a new database
+Create database bootcamp2026;
+-- show databases this is similar to showing all folders in computer
+show databases;
+
+SELECT * FROM bootcamp2026.customers;customers
+ 
+INSERT INTO bootcamp2026.customers (customer_id, customer_name)
+VALUES(1, 'Harsh');
