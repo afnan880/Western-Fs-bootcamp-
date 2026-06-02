723895cbe3d274156163dc2266e030a5f4d3a3fc
afnan <afnanmudsh@gmail.com>
2026-06-01 21:07:13 -0400

    sql

--- node/Untitled.sql
+++ node/Untitled.sql
@@ -1,9 +1,34 @@
--- to create a new database
+-- To create a new database. Think of database as creating a folder on your computer.
 Create database bootcamp2026;
--- show databases this is similar to showing all folders in computer
-show databases;
-
-SELECT * FROM bootcamp2026.customers;customers
  
-INSERT INTO bootcamp2026.customers (customer_id, customer_name)
-VALUES(1, 'Harsh');
+-- Show databases. This is similar to showing all the folders on a computer
+show databases;
+ 
+-- To create customers table with customer_id, customer_name and customer_email
+CREATE TABLE bootcamp2026.customers (
+  customer_id INT NOT NULL AUTO_INCREMENT,
+  customer_name VARCHAR(100) NOT NULL,
+  customer_email VARCHAR(100) NOT NULL,
+  PRIMARY KEY (customer_id),
+  UNIQUE INDEX customer_email_UNIQUE (customer_email ASC) VISIBLE);
+ 
+ 
+-- select all the columns from bootcamp2026 database's customers table. 
+SELECT * FROM bootcamp2026.customers;
+ 
+-- To add data to customers table with specified columns
+INSERT INTO bootcamp2026.customers ( customer_name, customer_email)
+VALUES('Test', 'test@gmail.com');
+ 
+-- Another way to insert data without columns but then you need to provide data for all the columns in same sequence
+INSERT INTO bootcamp2026.customers
+VALUES(2, 'Test', 'test@gmail.com');
+ 
+-- Insert multiple rows at once
+INSERT INTO bootcamp2026.customers (customer_name, customer_email)
+VALUES ('Andrew', 'andrew@gmail.com'),
+('Kobi', 'kobi@gmail.com'),
+('Afnan', 'afnan@gmail.com');
+ 
+ 
+DROP table bootcamp2026.customers;
